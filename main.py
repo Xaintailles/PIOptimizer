d14c144060c6d67e0bdfeeb9c9d4342f5c018d54
Xaintailles <calixte.allier@gmail.com>
2023-06-07 16:12:32 +0200

    wotking a bit on it

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ Created on Sun Feb 20 00:20:35 2022
 """
 
 import pandas as pd
+from resources import get_p_zero_requirements
 
 universeDump = pd.read_csv(r'C:\Users\Gebruiker\Downloads\mapDenormalize.csv')
 
@@ -35,8 +36,14 @@ allPlanetsCleaned['planetType'] = allPlanetsCleaned['planetType'].str.replace(')
 
 allSystemsCleaned = allPlanetsCleaned.groupby(['solarSystemID', 'constellationID', 'regionID','security','itemName','typeName']).size().to_frame(name = 'count').reset_index()
 
-
-#To Do: split the name of the system based on finding the first space after the number of the planet to get the system name
 fountain = allPlanetsCleaned[allPlanetsCleaned['regionID'] == 10000058]
 
+fountain['itemName'] = fountain.itemName.str.split(" ", expand = True)
+
 #Regionid to regionname: https://gist.github.com/mentos1386/8192dba4f124395c0d7f
+
+test = get_p_zero_requirements()
+
+print(test['test_results'])
+
+#TODO: work on getting the correct system now that you have a list of all the planets needed and all the planets present
